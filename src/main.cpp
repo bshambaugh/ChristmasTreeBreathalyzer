6d2b1abc62fb7d5423a51026abb762a0811a6468
Brent Shambaugh <brent.shambaugh@gmail.com>
2025-12-09 15:52:57 -0600

    backup modified code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,29 @@
 #include <Arduino.h>
+/* with the pull-up resistor*/
+
+
+int buttonPin = 8; //5
+int Led = 10;
+
+unsigned long time_value;
+
+void setup() {
+pinMode(buttonPin,INPUT);
+pinMode(Led,OUTPUT);
+Serial.begin(115200);
+}
+
+void loop() {
+int buttonState = digitalRead(buttonPin); //read the state of the button input
+if (buttonState == LOW) { //pressing the button will produce a LOW state 0V
+digitalWrite(Led,HIGH); //the led with turn on
+Serial.println(buttonState);
+} else{
+digitalWrite(Led,LOW); //the led with turn off
+} Serial.println(buttonState); //check in the serial monitor
+}
+
+/*
 
 void setup() {
    pinMode(LED_BUILTIN, HIGH);
@@ -14,3 +39,4 @@ void loop() {
   delay(1000);
 }
 
+*/
