54a9643d3fa4beb8ede91dbf32f79493b3d1ff60
Brent Shambaugh <brent.shambaugh@gmail.com>
2025-12-11 19:10:52 -0600

    remove commented out code

--- src/main.cpp
+++ src/main.cpp
@@ -35,20 +35,3 @@ void loop() {
     Serial.println(" ms");
   }
 }
-/*
-
-void setup() {
-   pinMode(LED_BUILTIN, HIGH);
-   Serial.begin(115200);
-}
-
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.print("I am high");
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.print("I am sober");
-  delay(1000);
-}
-
-*/
